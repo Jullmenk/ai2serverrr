5104fee0759aa349c59f17273d952fa79fd33c84
Jullmenk <julinolady@gmail.com>
2025-05-23 19:19:15 +0100

    Add logging for DATABASE_URL and clean up comments in movieRoutes

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,19 +3,13 @@ const router = express.Router();
 const movieController = require('../controllers/movieController');
 const multer = require('multer')
 const upload = multer({dest:'uploads/'})
-// List all movies
 router.get('/', movieController.movieList);
-
-// Get movie by ID
 router.get('/:id', movieController.movieDetail);
-
-// Create new movie
 router.post('/create', upload.single('image'), movieController.movieCreate);
 
-// Update movie
+
 router.put('/update/:id', upload.single('image'), movieController.movieUpdate);
 
-// Delete movie
 router.delete('/delete/:id', movieController.movieDelete);
 
 module.exports = router;
